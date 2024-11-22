02044ff730d7b4a126298931d836e0adbd8c578a
lrshsl <hoeslilars@gmail.com>
2024-11-23 00:11:23 +0100

    update main

--- main.py
+++ main.py
@@ -11,18 +11,22 @@ def main():
     assert len(args) >= 2, "pls provide a filename";
     filename: str = args[1];
 
-    # Reverse
     reverse: bool = False;
+    randomize_order: bool = False;
+
+    # Reverse
     if len(args) == 3:
         if args[2] in ("-rev", "--reverse-word-definition"):
             print("Word-definition inverted");
             reverse = True;
+        elif args[2] in ("-rand", "--randomize-order"):
+            print("Order of words randomized");
+            randomize_order = True;
         else:
             print(f"Argument at place 2 ({args[2]}) unknown");
             sys.exit(1);
 
     # Randomize order
-    randomize_order: bool = False;
     if len(args) == 4:
         if args[3] in ("-rand", "--randomize-order"):
             print("Order of words randomized");
